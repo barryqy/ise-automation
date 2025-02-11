ca81532c8b4778542e9185ca1a8be98764dabc71
barry <barryy@dr.com>
2025-02-11 13:26:07 +0100

    Update pods.py

--- .eXpo/pods.py
+++ .eXpo/pods.py
@@ -3,7 +3,7 @@ from expo import CiscoExpoApi  # make sure expo.py is present in the same folder
 api = CiscoExpoApi()
 
 # Access the eXpo
-expo = api.get_expo("aa2luz73bvbj3mg0lk3zoklzq")
+expo = api.get_expo("5ov2borpcrzj76vi2hfmw6rh9")
 print(f"Expo Name: {expo.name}")
 if api.choose_datacenter():
     # Create an engagement (replace with your email and demo UID)
